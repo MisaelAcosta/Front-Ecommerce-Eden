20cce87e84958d38bf97fbca257c42a627dd4f28
Misael Acosta <acostamisael166@gmail.com>
2026-04-25 16:44:03 -0400

    footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,56 +1,71 @@
 "use client";
 
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import Link from "next/link";
 
-const inter = Inter({
-  weight: ["400", "900"],
-  subsets: ["latin"],
+const maratype = localFont({
+  src: "./fonts/Maratype.otf",
+});
+
+const khInterferenceBold = localFont({
+  src: "./fonts/KHInterferenceTRIAL-Bold.otf",
+});
+
+const khInterferenceLight = localFont({
+  src: "./fonts/KHInterferenceTRIAL-Light.otf",
 });
 
 export default function Footer() {
   return (
-    <footer className="border-t border-gray-200 mt-10 bg-[#111111]">
-      {/* Contenido principal */}
-      <div className="max-w-7xl mx-auto px-8 md:px-10 py-5 md:py-8">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 lg:gap-12">
-          {/* Texto grande */}
+    <footer className="mt-10 border-t border-gray-200 bg-[#111111]">
+      <div className="mx-auto max-w-7xl px-8 py-5 md:px-10 md:py-8">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-5 lg:gap-12">
           <div className="md:col-span-2 lg:col-span-2">
             <h1
-              className={`${inter.className} text-white sm:text-start text-center font-black md:border-r tracking-tight text-5xl leading-10 lg:text-7xl lg:leading-[0.9]`}
+              className={`${maratype.className} text-center 
+              text-6xl leading-17 tracking-tight 
+              text-white sm:text-start md:border-r 
+              lg:text-8xl lg:leading-[1.10]`}
             >
               EL FUTURO <br />
-              ESTÁ <br />
+              ESTA <br />
               IMPRESO
             </h1>
           </div>
 
-          {/* Columna explorar */}
           <div className="col-span-1">
-            <h3 className="font-semibold mb-3 sm:text-start text-center text-base text-white md:text-lg">
+            <h3
+              className={`${khInterferenceBold.className} mb-3 
+              text-center text-lg text-white sm:text-start 
+              md:text-2xl`}
+            >
               Explora
             </h3>
-            <ul className="space-y-3 text-center sm:text-start">
+            <ul
+              className={`${khInterferenceLight.className} 
+              space-y-3 text-center sm:text-start`}
+            >
               <li>
                 <Link
                   href="/explora"
-                  className="text-gray-400 hover:text-white hover:underline transition-colors"
+                  className="text-gray-400 transition-colors
+                   hover:text-white hover:underline"
                 >
-                  Políticas de privacidad
+                  Politicas de privacidad
                 </Link>
               </li>
               <li>
                 <Link
                   href="/explora"
-                  className="text-gray-400 hover:text-white hover:underline transition-colors"
+                  className="text-gray-400 transition-colors hover:text-white hover:underline"
                 >
-                  Términos y Condiciones
+                  Terminos y Condiciones
                 </Link>
               </li>
               <li>
                 <Link
                   href="/explora"
-                  className="text-gray-400 hover:text-white hover:underline transition-colors"
+                  className="text-gray-400 transition-colors hover:text-white hover:underline"
                 >
                   Sobre Nosotros
                 </Link>
@@ -58,16 +73,18 @@ export default function Footer() {
             </ul>
           </div>
 
-          {/* Columna contacto */}
-          <div className="col-span-1 text-white text-center sm:text-start">
-            <h3 className="font-semibold mb-3 text-base md:text-lg">
-              Contáctanos
+          <div
+            className={`${khInterferenceLight.className} col-span-1 text-center text-white sm:text-start`}
+          >
+            <h3 className={`${khInterferenceBold.className} mb-3 
+            text-lg md:text-2xl`}>
+              Contactanos
             </h3>
             <ul className="space-y-3 text-center sm:text-start">
               <li>
                 <a
                   href="mailto:EdenCorreos@gmail.com"
-                  className="text-gray-400 hover:text-white hover:underline transition-colors break-all"
+                  className="break-all text-gray-400 transition-colors hover:text-white hover:underline"
                 >
                   Gmail
                 </a>
@@ -75,7 +92,7 @@ export default function Footer() {
               <li>
                 <a
                   href="tel:+931107284"
-                  className="text-gray-400 hover:text-white hover:underline transition-colors"
+                  className="text-gray-400 transition-colors hover:text-white hover:underline"
                 >
                   whatsapp
                 </a>
@@ -85,7 +102,7 @@ export default function Footer() {
                   href="https://www.instagram.com/eden.3d_/"
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="text-gray-400 hover:text-white hover:underline transition-colors"
+                  className="text-gray-400 transition-colors hover:text-white hover:underline"
                 >
                   Instagram
                 </a>
@@ -95,11 +112,12 @@ export default function Footer() {
         </div>
       </div>
 
-      {/* Parte inferior */}
       <div className="border-gray-200">
-        <div className="max-w-2xl ml-auto px-10 md:px-25 py-2">
-          <div className="flex sm:flex-row items-center justify-between text-xs md:text-sm text-gray-500 gap-2">
-            <p>© 2026 Eden.</p>
+        <div className="ml-auto max-w-2xl px-10 py-2 md:px-25">
+          <div
+            className={`${khInterferenceLight.className} flex items-center justify-between gap-2 text-xs text-gray-500 md:text-sm sm:flex-row`}
+          >
+            <p>&copy; 2026 Eden.</p>
             <p>Todos los derechos reservados</p>
           </div>
         </div>
